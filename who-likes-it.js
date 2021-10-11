e7cd5ab669fd275f36ce606a70236fdd1ed616a4
Pr0C33D <hero1393@gmail.com>
2021-10-11 12:21:53 +0300

    DONE

---
+++ who-likes-it.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5266876b8f4bf2da9b000362
+
+function likes (names) {
+    const templates = [
+      'no one likes this',
+      '{name} likes this',
+      '{name} and {name} like this',
+      '{name}, {name} and {name} like this',
+      '{name}, {name} and {n} others like this'
+    ];
+    let idx = Math.min(names.length, 4);
+    
+    return templates[idx].replace(/{name}|{n}/g, function (val) {
+      return val === '{name}' ? names.shift() : names.length;
+    });
+  }
